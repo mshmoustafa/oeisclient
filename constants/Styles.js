dd4b87ec1a10528cf634605797640e481a1faf1d
Muhammad-Sharif Moustafa <mshm@protonmail.com>
2019-05-24 01:27:41 -0400

    Use Menlo font for monospace on iOS

--- constants/Styles.js
+++ constants/Styles.js
@@ -1,9 +1,9 @@
-import React, {StyleSheet} from 'react-native'
+import React, {StyleSheet, Platform} from 'react-native'
 import Colors from './Colors';
 
 export default StyleSheet.create({
   monospace: {
-    fontFamily: "space-mono",
+    fontFamily: Platform.OS === "ios" ? "Menlo" : "space-mono",
   },
   active: {
     color: Colors.tintColor,
