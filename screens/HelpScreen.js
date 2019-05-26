65f10866fb724311dce80cad4b594a864628615a
Muhammad-Sharif Moustafa <mshm@protonmail.com>
2019-05-26 08:03:13 -0400

    Add bugs section to HelpScreen

--- screens/HelpScreen.js
+++ screens/HelpScreen.js
@@ -51,6 +51,11 @@ export default class HelpScreen extends React.Component {
         body: <Text>Searching in this app is very similar to searching on the OEIS website. For example, to search for the sequence with an ID of <Text style={Styles.monospace}>A000045</Text>, type <Text style={Styles.monospace}>id:A000045</Text> in the search bar and tap Search. For more info on searching, refer to the OEIS hints webpage.</Text>
       },
       {
+        key: 3,
+        header: "Bugs?",
+        body: <Text>If you experience any bugs, need help, or have any feedback, let me know by sending an email to <LinkText href="mailto:mshm@protonmail.com">mshm@protonmail.com</LinkText> with "OEIS Client" in the subject.</Text>
+      },
+      {
         key: 2,
         header: "About",
         body: <Text>Neither this app nor the developer are affiliated with the Online Encyclopedia of Integers (<LinkText href="https://oeis.org">https://oeis.org</LinkText>). All of the sequences and related information are retrieved from the OEIS. OEIS and THE ON-LINE ENCYCLOPEDIA OF INTEGER SEQUENCES are trademarks of The OEIS Foundation Inc. More information can be found at <LinkText href="https://oeisf.org">https://oeisf.org</LinkText>.</Text>
