e9e34846f6a6466a6c5053f7ab8f45724d52993b
Muhammad-Sharif Moustafa <mshm@protonmail.com>
2019-05-26 11:42:22 -0400

    Render programs in monospace and in a single cell

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -10,6 +10,7 @@ import SimpleList from '../components/List';
 import { ListCell, ListCellSeparator } from '../components/ListCell';
 import Utility from '../lib/Utility';
 import {OEIS, OEISResponse, OEISSequence} from "../lib/oeis";
+import Styles from '../constants/Styles.js';
 
 export default class DetailScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -23,34 +24,56 @@ export default class DetailScreen extends React.Component {
     let propertyString = this.props.navigation.getParam("property");
     if (propertyString === "maple" || propertyString === "mathematica" || propertyString === "program") {
       // Use monospace font and maybe some special formatting
+      let key = 0;
+      let cellContents = "";
+      let items = this.props.navigation.getParam("items");
+      if (items !== null && items !== undefined && items.length > 0) {
+        items.forEach((element) => {
+          cellContents = cellContents + element + "\n";
+        });
+        return (
+          <FlatList
+            style={styles.base}
+            data={[{key: key, body: cellContents}]}
+            renderItem={this._renderProgram}
+            ItemSeparatorComponent={() => <ListCellSeparator />}
+            keyExtractor={ (item) => item.key.toString() } />
+        );
+      } else {
+        return (
+          <View style={styles.noEntriesView}>
+            <Text style={styles.noEntriesText}>No entries</Text>
+          </View>
+        );
+      }
     } else {
       // Use normal font and normal formatting
-    }
-    let key = 0;
-    let cellContents = [];
-    let items = this.props.navigation.getParam("items");
-    if (items !== null && items !== undefined && items.length > 0) {
-      items.forEach((element) => {
-        cellContents.push({
-          key: key,
-          body: element,
+      let key = 0;
+      let cellContents = [];
+      let items = this.props.navigation.getParam("items");
+      if (items !== null && items !== undefined && items.length > 0) {
+        items.forEach((element) => {
+          cellContents.push({
+            key: key,
+            body: element,
+          })
+          key = key + 1;
         })
-        key = key + 1;
-      })
-      return (
-        <FlatList
-          style={styles.base}
-          data={cellContents}
-          renderItem={this._renderItem}
-          ItemSeparatorComponent={() => <ListCellSeparator />}
-          keyExtractor={ (item) => item.key.toString() } />
-      );
-    } else {
-      return (
-        <View style={styles.noEntriesView}>
-          <Text style={styles.noEntriesText}>No entries</Text>
-        </View>
-      );
+        return (
+          <FlatList
+            style={styles.base}
+            data={cellContents}
+            renderItem={this._renderItem}
+            ItemSeparatorComponent={() => <ListCellSeparator />}
+            keyExtractor={ (item) => item.key.toString() } />
+        );
+      } else {
+        return (
+          <View style={styles.noEntriesView}>
+            <Text style={styles.noEntriesText}>No entries</Text>
+          </View>
+        );
+      }
     }
   }
 
@@ -77,6 +100,20 @@ export default class DetailScreen extends React.Component {
     let body = (
       <View>
         <Text selectable={true}>{bodyWithParsedLinksAndSequencesAndAuthors}</Text>
+      </View>
+    )
+    return (
+      <ListCell
+        style={styles.listCell}
+        key={item.key}
+        body={body} />
+    );
+  }
+
+  _renderProgram = ({item}) => {
+    let body = (
+      <View>
+        <Text selectable={true} style={Styles.monospace}>{item.body}</Text>
       </View>
     )
     return (
