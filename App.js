26a7ea7282c92fa072e628771b56f753c3d87a02
Muhammad-Sharif Moustafa <mshm@protonmail.com>
2019-05-26 07:07:30 -0400

    Delete stale comments about SpaceMono - We actually do use SpaceMono in the app

--- App.js
+++ App.js
@@ -32,8 +32,6 @@ export default class App extends React.Component {
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
       }),
     ]);
