688db5e3fd17f21edb1dee5eca1835aefed2e335
Muhammad-Sharif Moustafa <mshm@protonmail.com>
2019-05-26 07:09:11 -0400

    Delete Asset import

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import { AppLoading, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
